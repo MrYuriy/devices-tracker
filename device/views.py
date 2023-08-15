8876bd51e304ff72f6ee5ac1a95e514cf3a69637
YuriiBabiuk <118677914+YuriiBabiuk@users.noreply.github.com>
2023-08-15 17:23:57 +0300

    Only login user have access

--- device/views.py
+++ device/views.py
@@ -3,7 +3,7 @@ from django.views import generic
 from django.urls import reverse_lazy
 
 from device.forms import DeviceDepartmentForm, DevicePortForm, DeviceIPForm
-
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import (
     Device,
     DeviceDepartment,
@@ -16,27 +16,27 @@ from .models import (
 
 
 # Device Site
-class DeviceSiteListView(generic.ListView):
+class DeviceSiteListView(LoginRequiredMixin, generic.ListView):
     model = DeviceSite
     template_name = "device/device_site_list.html"
     context_object_name = "device_site_list"
 
 
-class DeviceSiteCreateView(generic.CreateView):
+class DeviceSiteCreateView(LoginRequiredMixin, generic.CreateView):
     model = DeviceSite
     fields = "__all__"
     success_url = reverse_lazy("device:device-site-list")
     template_name = "device/device_site_form.html"
 
 
-class DeviceSiteUpdateView(generic.UpdateView):
+class DeviceSiteUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = DeviceSite
     fields = "__all__"
     success_url = reverse_lazy("device:device-site-list")
     template_name = "device/device_site_form.html"
 
 
-class DeviceSiteDeleteView(generic.DeleteView):
+class DeviceSiteDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = DeviceSite
     fields = "__all__"
     success_url = reverse_lazy("device:device-site-list")
@@ -45,54 +45,54 @@ class DeviceSiteDeleteView(generic.DeleteView):
 
 # Device Department
 
-class DeviceDepartmentListView(generic.ListView):
+class DeviceDepartmentListView(LoginRequiredMixin, generic.ListView):
     model = DeviceDepartment
     template_name = "device/device_department_list.html"
     context_object_name = "device_department_list"
 
 
-class DeviceDepartmentCreateView(generic.CreateView):
+class DeviceDepartmentCreateView(LoginRequiredMixin, generic.CreateView):
     model = DeviceDepartment
     form_class = DeviceDepartmentForm
     success_url = reverse_lazy("device:device-department-list")
     template_name = "device/device_department_form.html"
 
 
-class DeviceDepartmentUpdateView(generic.UpdateView):
+class DeviceDepartmentUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = DeviceDepartment
     form_class = DeviceDepartmentForm
     success_url = reverse_lazy("device:device-department-list")
     template_name = "device/device_department_form.html"
 
 
-class DeviceDepartmentDeleteView(generic.DeleteView):
+class DeviceDepartmentDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = DeviceDepartment
     success_url = reverse_lazy("device:device-department-list")
     template_name = "device/device_department_confirm_delete.html"
 
 
 # Device Status
-class DeviceStatusListView(generic.ListView):
+class DeviceStatusListView(LoginRequiredMixin, generic.ListView):
     model = DeviceStatus
     template_name = "device/device_status_list.html"
     context_object_name = "device_status_list"
 
 
-class DeviceStatusCreateView(generic.CreateView):
+class DeviceStatusCreateView(LoginRequiredMixin, generic.CreateView):
     model = DeviceStatus
     fields = "__all__"
     success_url = reverse_lazy("device:device-status-list")
     template_name = "device/device_status_form.html"
 
 
-class DeviceStatusUpdateView(generic.UpdateView):
+class DeviceStatusUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = DeviceStatus
     fields = "__all__"
     success_url = reverse_lazy("device:device-status-list")
     template_name = "device/device_status_form.html"
 
 
-class DeviceStatusDeleteView(generic.DeleteView):
+class DeviceStatusDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = DeviceStatus
     success_url = reverse_lazy("device:device-status-list")
     template_name = "device/device_status_confirm_delete.html"
@@ -100,82 +100,82 @@ class DeviceStatusDeleteView(generic.DeleteView):
 
 # Device Type
 
-class DeviceTypeListView(generic.ListView):
+class DeviceTypeListView(LoginRequiredMixin, generic.ListView):
     model = DeviceType
     success_url = reverse_lazy("device:device-type-list")
     template_name = "device/device_type_list.html"
     context_object_name = "device_type_list"
 
 
-class DeviceTypeCreateView(generic.CreateView):
+class DeviceTypeCreateView(LoginRequiredMixin, generic.CreateView):
     model = DeviceType
     fields = "__all__"
     success_url = reverse_lazy("device:device-type-list")
     template_name = "device/device_type_form.html"
 
 
-class DeviceTypeUpdateView(generic.UpdateView):
+class DeviceTypeUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = DeviceType
     fields = "__all__"
     success_url = reverse_lazy("device:device-type-list")
     template_name = "device/device_type_form.html"
 
 
-class DeviceTypeDeleteView(generic.DeleteView):
+class DeviceTypeDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = DeviceType
     success_url = reverse_lazy("device:device-type-list")
     template_name = "device/device_type_confirm_delete.html"
 
 
 # Device
-class DeviceListView(generic.ListView):
+class DeviceListView(LoginRequiredMixin, generic.ListView):
     model = Device
     template_name = "device/device_list.html"
     context_object_name = "device_list"
 
 
-class DeviceCreateView(generic.CreateView):
+class DeviceCreateView(LoginRequiredMixin, generic.CreateView):
     model = Device
     fields = "__all__"
     success_url = reverse_lazy("device:device-list")
     template_name = "device/device_form.html"
 
 
-class DeviceUpdateView(generic.UpdateView):
+class DeviceUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Device
     fields = "__all__"
     success_url = reverse_lazy("device:device-list")
     template_name = "device/device_form.html"
 
 
-class DeviceDeleteView(generic.DeleteView):
+class DeviceDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Device
     success_url = reverse_lazy("device:device-list")
     template_name = "device/device_confirm_delete.html"
 
 
 # Device Port
-class DevicePortListView(generic.ListView):
+class DevicePortListView(LoginRequiredMixin, generic.ListView):
     model = DevicePort
     template_name = "device/device_port_list.html"
     context_object_name = "device_port_list"
 
 
-class DevicePortCreateView(generic.CreateView):
+class DevicePortCreateView(LoginRequiredMixin, generic.CreateView):
     model = DevicePort
     form_class = DevicePortForm
     success_url = reverse_lazy("device:device-port-list")
     template_name = "device/device_port_form.html"
 
 
-class DevicePortUpdateView(generic.UpdateView):
+class DevicePortUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = DevicePort
     form_class = DevicePortForm
     success_url = reverse_lazy("device:device-port-list")
     template_name = "device/device_port_form.html"
 
 
-class DevicePortDeleteView(generic.DeleteView):
+class DevicePortDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = DevicePort
     success_url = reverse_lazy("device:device-port-list")
     template_name = "device/device_port_confirm_delete.html"
@@ -183,27 +183,27 @@ class DevicePortDeleteView(generic.DeleteView):
 
 # Device IP
 
-class DeviceIPListView(generic.ListView):
+class DeviceIPListView(LoginRequiredMixin, generic.ListView):
     model = DeviceIP
     template_name = "device/device_ip_list.html"
     context_object_name = "device_ip_list"
 
 
-class DeviceIPCreateView(generic.CreateView):
+class DeviceIPCreateView(LoginRequiredMixin, generic.CreateView):
     model = DeviceIP
     form_class = DeviceIPForm
     success_url = reverse_lazy("device:device-ip-list")
     template_name = "device/device_ip_form.html"
 
 
-class DeviceIPUpdateView(generic.UpdateView):
+class DeviceIPUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = DeviceIP
     form_class = DeviceIPForm
     success_url = reverse_lazy("device:device-ip-list")
     template_name = "device/device_ip_form.html"
 
 
-class DeviceIPDeleteView(generic.DeleteView):
+class DeviceIPDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = DeviceIP
     success_url = reverse_lazy("device:device-ip-list")
     template_name = "device/device_ip_confirm_delete.html"
