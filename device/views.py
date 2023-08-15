974439bddefe5964eb362cd0f84e2fb09840ce53
YuriiBabiuk <118677914+YuriiBabiuk@users.noreply.github.com>
2023-08-15 22:14:39 +0300

    Add posible to filtering with status type and department

--- device/views.py
+++ device/views.py
@@ -138,6 +138,33 @@ class DeviceListView(LoginRequiredMixin, generic.ListView):
     template_name = "device/device_list.html"
     context_object_name = "device_list"
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        status_id = self.request.GET.get("status")
+        if status_id:
+            status = get_object_or_404(DeviceStatus, pk=status_id)
+            queryset = queryset.filter(device_status=status)
+
+        device_type_id = self.request.GET.get("device_type")
+        if device_type_id:
+            device_type = get_object_or_404(DeviceType, pk=device_type_id)
+            queryset = queryset.filter(device_type=device_type)
+
+        department_id = self.request.GET.get("department")
+        if department_id:
+            department = get_object_or_404(DeviceDepartment, pk=department_id)
+            queryset = queryset.filter(department=department)
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['device_status_list'] = DeviceStatus.objects.all()
+        context['device_type_list'] = DeviceType.objects.all()
+        context['department_list'] = DeviceDepartment.objects.all()
+        return context
+
 
 class DeviceCreateView(LoginRequiredMixin, generic.CreateView):
     model = Device
