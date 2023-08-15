40377298d4b265b54adcbf6a4d9e893d9b29bdf4
Bohdan11Dii <bodyabrovdiy@gmail.com>
2023-08-15 17:58:58 +0300

    Create registration page

--- device_tracker/settings.py
+++ device_tracker/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 AUTH_USER_MODEL = "user.User"
-
+LOGIN_REDIRECT_URL = "/devices/"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
