48ed32b4f421232b70f7ae6a635bfc288f6b22dd
YuriiBabiuk <118677914+YuriiBabiuk@users.noreply.github.com>
2023-08-22 09:41:06 +0300

    Add ALLOWED_HOSTS

--- device_tracker/settings.py
+++ device_tracker/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "devtracker.onrender.com/"]
+ALLOWED_HOSTS = ["127.0.0.1", "devtracker.onrender.com"]
 
 SPREADSHEET_ID = os.environ["SPREADSHEET_ID"]
 # Application definition
